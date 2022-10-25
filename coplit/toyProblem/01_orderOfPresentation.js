9e587e707038b84b03aa68ce1cbaf1d6afcea0e2
natreeum <projectkmscom@gmail.com>
2022-10-25 14:59:51 +0900

    commit

---
+++ coplit/toyProblem/01_orderOfPresentation.js
@@ -0,0 +1,36 @@
+function orderOfPresentation(N, K) {
+  // TODO: 여기에 코드를 작성합니다.
+  // N : nums length
+  // arr : finding arr
+  let result = 0;
+  let nums = []; //1,2,3,4,5
+
+  function fact(num) {
+    if (num <= 1) return num;
+    return fact(num - 1) * num;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    nums.push(i);
+  }
+
+  for (let i = 1; i <= N; i++) {
+    let cnt = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (K[i - 1] > nums[j]) cnt++;
+    }
+    for (let j of nums) {
+      if (K[i - 1] === j) {
+        nums.splice(nums.indexOf(j), 1);
+      }
+    }
+    result += cnt * fact(N - i);
+  }
+  return result;
+}
+
+let output = orderOfPresentation(3, [2, 3, 1]);
+console.log(output); // 3
+
+output = orderOfPresentation(5, [1, 3, 2, 4, 5]);
+console.log(output); // 6
