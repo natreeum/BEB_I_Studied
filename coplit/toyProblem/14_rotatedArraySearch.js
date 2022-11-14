58eb7d2aace1e1f96e05932ab6e1e0ef60e353e7
natreeum <projectkmscom@gmail.com>
2022-11-14 22:05:56 +0900

    edit:toy_14

--- coplit/toyProblem/14_rotatedArraySearch.js
+++ coplit/toyProblem/14_rotatedArraySearch.js
@@ -1,24 +1,29 @@
 const rotatedArraySearch = function (rotated, target) {
   // TODO : 여기에 코드를 작성합니다.
   let min = 0;
-  let max = rotated.length;
+  let max = rotated.length - 1;
 
-  while (1) {
-    let midIndex = Math.floor((min + max) / 2);
+  while (min <= max) {
+    const midIndex = Math.floor((min + max) / 2);
     if (rotated[midIndex] === target) return midIndex;
-    if (rotated[midIndex] > target) {
-      max = midIndex - 1;
+    // mid index is in right side Array
+    if (rotated[midIndex] <= rotated[max]) {
+      if (rotated[midIndex] < target && rotated[max] >= target) {
+        min = midIndex + 1;
+      } else max = midIndex - 1;
     }
-    if (rotated[midIndex] < target) {
-      if (rotated[rotated.length - 1] < target) max = midIndex - 1;
-      else min = midIndex + 1;
+    // mid index is in left side Array
+    else {
+      if (rotated[midIndex] > target && rotated[min] <= target) {
+        max = midIndex - 1;
+      } else min = midIndex + 1;
     }
-    if (rotated[midIndex] === undefined) return -1;
   }
+  return -1;
 };
 
-let output = rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2);
+let output = rotatedArraySearch([10, 11, 12, 3, 6, 7, 8, 9], 11);
 console.log(output); // --> 5
 
-output = rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100);
+output = rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 1);
 console.log(output); // --> -1
