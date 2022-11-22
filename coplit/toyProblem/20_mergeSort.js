8d9d269502f491a47cfcc86450719fce2e013d2b
natreeum <projectkmscom@gmail.com>
2022-11-22 11:38:30 +0900

    edit:toy_20

--- coplit/toyProblem/20_mergeSort.js
+++ coplit/toyProblem/20_mergeSort.js
@@ -1,22 +1,20 @@
 const mergeSort = function (arr) {
-  if (arr.length <= 1) return arr;
-
-  let mid = Math.ceil(arr.length / 2);
-  let left = arr.slice(0, mid);
-  let right = arr.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
-};
-function merge(left, right) {
-  let result = [];
-
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      result.push(left.shift());
-    } else {
-      result.push(right.shift());
+  const merge = (leftArr, rightArr) => {
+    let result = [];
+    while (leftArr.length != 0 && rightArr.length != 0) {
+      if (leftArr[0] < rightArr[0]) {
+        result.push(leftArr.shift());
+      } else result.push(rightArr.shift());
     }
-  }
+    return result.concat(leftArr, rightArr);
+  };
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
 
-  return [...result, ...left, ...right];
-}
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+};
+
+const arr = [5, 4, 3, 2, 1];
+console.log(mergeSort(arr));
