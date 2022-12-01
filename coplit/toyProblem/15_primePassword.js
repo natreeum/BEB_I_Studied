45e8617a10c688e933a6da821133b9e9b1e41ed0
natreeum <projectkmscom@gmail.com>
2022-12-01 15:35:40 +0900

    edit : solve toy_15

--- coplit/toyProblem/15_primePassword.js
+++ coplit/toyProblem/15_primePassword.js
@@ -1,30 +1,38 @@
 const primePassword = (curPwd, newPwd) => {
   // TODO: 여기에 코드를 작성합니다.
-  const isPrime = (num) => {
-    for (let i = 2; i < num; i++) {
-      if (i * i > num) break;
-      if (num % i === 0) return false;
+  const isPrime = Array(10000).fill(true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (let i = 2; i * i < isPrime.length; i++) {
+    for (let j = i * i; j < isPrime.length; j += i) {
+      if (isPrime[j]) {
+        isPrime[j] = false;
+      }
     }
-    return true;
-  };
+  }
 
-  const numToArr = (num) => {
-    let arr = [];
-    arr[0] = Math.floor(num / 1000);
-    num = num % 1000;
-    arr[1] = Math.floor(num / 100);
-    num = num % 100;
-    arr[2] = Math.floor(num / 10);
-    num = num % 10;
-    arr[3] = num;
-    return arr;
-  };
-
-  const arrToNum = (arr) => {
-    return Number(arr.join(''));
-  };
-
-  let cnt = 0;
+  curPwd = String(curPwd)
+    .split('')
+    .map((e) => Number(e));
+  const queue = [[[...curPwd], 0]];
+  const visited = [];
+  while (queue.length != 0) {
+    const curVertex = queue.shift();
+    visited.push(curVertex[0].join(''));
+    if (Number(curVertex[0].join('')) === newPwd) return curVertex[1];
+    for (let i = 0; i < 4; i++) {
+      const compareNum = [...curVertex[0]];
+      for (let j = 0; j < 10; j++) {
+        if ((i === 0 && j === 0) || curVertex[0][i] === j) continue;
+        compareNum[i] = j;
+        const changedNum = Number(compareNum.join(''));
+        if (isPrime[changedNum] && !visited.includes(String(changedNum))) {
+          queue.push([[...compareNum], curVertex[1] + 1]);
+          visited.push(String(changedNum));
+        }
+      }
+    }
+  }
 };
 
-console.log(primePassword(1, 1));
+console.log(primePassword(9787, 9923));
